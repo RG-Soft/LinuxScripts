5efb18e5d84c903098fd33fd5c75369ac370611e
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-03-31 12:08:11 +0300

    Переработан модуль. Теперь модуль запускается с параметрами из инициализирующего запуск модуля.

--- pg_scripts/backup_dbname.sh
+++ pg_scripts/backup_dbname.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-dbname=1CHelper
-hostname=localhost
-port=5435
-username=postgres
-jobs=3
-backupdir=/pgbackup/$dbname/"$dbname"_now
-backupdir_inprogress=$backupdir.backuping
+#
+# RGS бэкапирование базы кластера
+#
+
+USAGE_STRING="Usage: $0 hosthame port dbname username jobs backupdir
+example: ./backup_dbname.sh srv01 5432 db_saler postgres 3 /pgbackup"
+
+hostname=$1
+port=$2
+dbname=$3
+username=$4
+jobs=$5
+backupdir_root=$6
+
+if [ -d "$backupdir_root" ]; then
+    echo "Отсутствует корневой каталог бэкапов! Создайте и настройте корневой каталог $backupdir_root..."
+    exit 100
+fi
+
+backupdir=/${backupdir_root}/${dbname}/${dbname}_now
+backupdir_inprogress=${backupdir}.backuping
 
 if [ -d "$backupdir_inprogress" ]; then
     echo "Бэкап уже выполняется! Если был прерван - удалите каталог $backupdir_inprogress вручную"
-    exit 100
+    exit 101
 fi
 
 if [ ! -d "$backupdir" ]; then

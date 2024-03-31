48f2f01b9a6fb40a4f76b61961d1ffecf8c662ac
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-03-31 23:49:34 +0300

    Исправлены имена шаблона регламента бэкапов, доработана проверка наличия параметров запуска

---
+++ pg_scripts/backup_dbase.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#
+# RGS бэкапирование базы кластера
+#
+
+USAGE_STRING="Usage: $0 hosthame port dbname username jobs backupdir
+example: ./backup_dbname.sh srv01 5432 db_saler postgres 3 /pgbackup"
+
+if [ "$1" == "" ]; then
+    echo "$USAGE_STRING"
+    exit 1
+fi
+
+hostname=$1
+port=$2
+dbname=$3
+username=$4
+jobs=$5
+backupdir_root=$6
+
+if [ -d "$backupdir_root" ]; then
+    echo "Отсутствует корневой каталог бэкапов! Создайте и настройте корневой каталог $backupdir_root..."
+    exit 100
+fi
+
+backupdir=/${backupdir_root}/${dbname}/${dbname}_now
+backupdir_inprogress=${backupdir}.backuping
+
+if [ -d "$backupdir_inprogress" ]; then
+    echo "Бэкап уже выполняется! Если был прерван - удалите каталог $backupdir_inprogress вручную"
+    exit 101
+fi
+
+if [ ! -d "$backupdir" ]; then
+    mkdir -p $backupdir_inprogress
+else
+    rm -f $backupdir/*
+    mv $backupdir $backupdir_inprogress
+fi
+
+echo "--==Start  $dbname backup==--"
+pg_dump --host $hostname --port $port --username $username --no-password --format directory --jobs $jobs --blobs --encoding UTF8 --verbose --file $backupdir_inprogress $dbname
+echo "--==Finish $dbname backup==--"
+
+mv $backupdir_inprogress $backupdir
--- pg_scripts/backup_dbname.sh
+++ pg_scripts/backup_dbname.sh
@@ -1,41 +1,16 @@
 #!/bin/bash
 
 #
-# RGS бэкапирование базы кластера
+# RGS Стартер бэкапирование базы кластера
 #
+#Пример запуска стартера:            ./backup_dbname.sh
+#Прмиер запуска исполняемого модуля: ./backup_dbase.sh srv01 5432 DBName postgres 3 /pgbackup"
 
-USAGE_STRING="Usage: $0 hosthame port dbname username jobs backupdir
-example: ./backup_dbname.sh srv01 5432 db_saler postgres 3 /pgbackup"
+dbname=DBase
+hostname=localhost
+port=5435
+username=postgres
+jobs=3
+backupdir_root=/pgbackup
 
-hostname=$1
-port=$2
-dbname=$3
-username=$4
-jobs=$5
-backupdir_root=$6
-
-if [ -d "$backupdir_root" ]; then
-    echo "Отсутствует корневой каталог бэкапов! Создайте и настройте корневой каталог $backupdir_root..."
-    exit 100
-fi
-
-backupdir=/${backupdir_root}/${dbname}/${dbname}_now
-backupdir_inprogress=${backupdir}.backuping
-
-if [ -d "$backupdir_inprogress" ]; then
-    echo "Бэкап уже выполняется! Если был прерван - удалите каталог $backupdir_inprogress вручную"
-    exit 101
-fi
-
-if [ ! -d "$backupdir" ]; then
-    mkdir -p $backupdir_inprogress
-else
-    rm -f $backupdir/*
-    mv $backupdir $backupdir_inprogress
-fi
-
-echo "--==Start  $dbname backup==--"
-pg_dump --host $hostname --port $port --username $username --no-password --format directory --jobs $jobs --blobs --encoding UTF8 --verbose --file $backupdir_inprogress $dbname
-echo "--==Finish $dbname backup==--"
-
-mv $backupdir_inprogress $backupdir
+./backup_dbase.sh $hosthame $port $dbname $username $jobs $backupdir_root

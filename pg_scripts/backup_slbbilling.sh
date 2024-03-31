a5545dbbdf287f063dac595ddd4a17357583211f
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-01 00:25:10 +0300

    Восстановлены стартеры для баз кластеров

---
+++ pg_scripts/backup_slbbilling.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# RGS Стартер бэкапирование базы кластера
+#
+#Пример запуска стартера:            ./backup_dbname.sh
+#Прмиер запуска исполняемого модуля: ./backup_dbase.sh srv01 5432 DBName postgres 3 /pgbackup"
+
+dbname=slbBilling
+hostname=localhost
+port=5434
+username=postgres
+jobs=3
+backupdir_root=/pgbackup
+
+./backup_dbase.sh $hosthame $port $dbname $username $jobs $backupdir_root

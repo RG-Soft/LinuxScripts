62da157987f84b5ec71907c32bb301105c5c69f9
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-01 12:32:37 +0300

    Реализован регламент vacuumdb со стартером

--- pg_scripts/vacuumdb_weekly_dbase.sh
+++ pg_scripts/vacuumdb_weekly_dbase.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-dbname=1CHelper
-hostname=localhost
-port=5435
-username=postgres
-jobs=3
+#
+# RGS регламент vacuumdb еженедельный базы кластера
+#
+
+USAGE_STRING="Usage: $0 hosthame port dbname username jobs
+example: ./backup_dbname.sh srv01 5432 db_saler postgres 3"
+
+if [ "$1" == "" ]; then
+    echo "$USAGE_STRING"
+    exit 1
+fi
+
+#test param
+#echo "$@"
+
+srvname=$1
+port=$2
+dbname=$3
+username=$4
+jobs=$5
 
 echo "--==Start  $dbname vacuum weekly==--"
-vacuumdb --host $hostname --port $port --username $username --no-password --jobs $jobs $dbname --analyze --freeze
+vacuumdb --host $srvname --port $port --username $username --no-password --jobs $jobs $dbname --analyze --freeze
 echo "--==Finish $dbname vacuum weekly==--"

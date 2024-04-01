c390afddf810217b6f6074ac2aafb4dfb6b68abe
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-01 19:17:51 +0300

    Добавлены модули vacuumdb_daily. Установка LF

---
+++ pg_scripts/vacuumdb_daily_slbbilling.sh
@@ -0,0 +1,14 @@
+
+#
+# RGS Стартер ежедневного регламента vacuumdb базы кластера
+#
+#Пример запуска стартера:            ./vacuumdb_dbname.sh
+#Прмиер запуска исполняемого модуля: ./vacuumdb_dbase.sh srv01 5432 DBName postgres 3"
+
+dbname=slbBilling
+srvname=localhost
+port=5434
+username=postgres
+jobs=3
+
+/var/lib/postgresql/vacuumdb_dbase.sh $srvname $port $dbname $username $jobs
---
+++ pg_scripts/vacuumdb_daily_tpm_wo.sh
@@ -0,0 +1,14 @@
+
+#
+# RGS Стартер ежедневного регламента vacuumdb базы кластера
+#
+#Пример запуска стартера:            ./vacuumdb_dbname.sh
+#Прмиер запуска исполняемого модуля: ./vacuumdb_dbase.sh srv01 5432 DBName postgres 3"
+
+dbname=TPM_WO
+srvname=localhost
+port=5433
+username=postgres
+jobs=3
+
+/var/lib/postgresql/vacuumdb_dbase.sh $srvname $port $dbname $username $jobs

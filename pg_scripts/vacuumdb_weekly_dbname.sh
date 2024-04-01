30f5242b8c1e51e3943434d8831fc93918e0a007
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-01 12:32:37 +0300

    Реализован регламент vacuumdb со стартером

---
+++ pg_scripts/vacuumdb_weekly_dbname.sh
@@ -0,0 +1,14 @@
+
+#
+# RGS Стартер регламента vacuumdb еженедельный базы кластера
+#
+#Пример запуска стартера:            ./vacuumdb_dbname.sh
+#Прмиер запуска исполняемого модуля: ./vacuumdb_dbase.sh srv01 5432 DBName postgres 3"
+
+dbname=DBase
+srvname=localhost
+port=5432
+username=postgres
+jobs=3
+
+./vacuumdb_dbase.sh $srvname $port $dbname $username $jobs

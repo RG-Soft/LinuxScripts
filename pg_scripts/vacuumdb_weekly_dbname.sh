957dd692b86961393af6b0f9f9acddcd3722a3ac
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-01 18:56:05 +0300

    Переработка основного модуля vacuumdb_dbase под запуск ежедневный и еженедельный

--- pg_scripts/vacuumdb_weekly_dbname.sh
+++ pg_scripts/vacuumdb_weekly_dbname.sh
@@ -1,14 +1,15 @@
 
 #
-# RGS Стартер регламента vacuumdb еженедельный базы кластера
+# RGS Стартер еженедельного регламента vacuumdb базы кластера
 #
 #Пример запуска стартера:            ./vacuumdb_dbname.sh
-#Прмиер запуска исполняемого модуля: ./vacuumdb_dbase.sh srv01 5432 DBName postgres 3"
+#Прмиер запуска исполняемого модуля: ./vacuumdb_dbase.sh srv01 5432 DBName postgres 3 freeze"
 
 dbname=DBase
 srvname=localhost
 port=5432
 username=postgres
 jobs=3
+freeze=freeze
 
-./vacuumdb_dbase.sh $srvname $port $dbname $username $jobs
+./vacuumdb_dbase.sh $srvname $port $dbname $username $jobs $freeze

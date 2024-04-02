32f21a981568902741779ebc74cc65eaad09e348
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-02 22:22:26 +0300

    Добавлены стартеры для кластеров сервера app53

---
+++ pg_scripts/repack_cluster_5434.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# RGS Стартер регламента repack всех баз кластера. НО ТОЛЬКО "убитых" таблиц и индексов
+#
+#Пример запуска стартера:            ./repack_5432.sh
+#Прмиер запуска исполняемого модуля: ./repack_cluster.sh srv01 5432 postgres 3"
+
+srvname=localhost
+port=5434
+username=postgres
+jobs=3
+
+./repack_cluster.sh $srvname $port $username $jobs

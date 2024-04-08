67e8a06f7cd962d4a29188aed64c06160479a23c
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-09 00:38:33 +0300

    Подготовлена часть модулей

---
+++ pg_scripts/vacuumdb_weekly_cluster_5432.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# RGS Стартер модуля регламента vacuumdb еженедельного для всех баз кластера.
+#
+#Пример запуска модуля:              ./vacuumdb_weekly_cluster_5432.sh
+#Прмиер запуска исполняемого модуля: ./vacuumdb_cluster.sh srv01 5432 postgres FREEZE"
+
+srvname=localhost
+port=5432
+username=postgres
+jobs=3
+freeze=FREEZE
+
+/var/lib/postgresql/vacuum_cluster.sh $srvname $port $username $jobs $freeze

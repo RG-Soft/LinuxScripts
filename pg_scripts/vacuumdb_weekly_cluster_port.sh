8c3729ef1c44a8b248d0ab1db6cf87bb0b403ae9
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-10 01:21:29 +0300

    Исправлены косяки и другой метод вызова vacuum

--- pg_scripts/vacuumdb_weekly_cluster_port.sh
+++ pg_scripts/vacuumdb_weekly_cluster_port.sh
@@ -3,7 +3,7 @@
 # RGS Стартер модуля регламента vacuumdb еженедельного для всех баз кластера.
 #
 #Пример запуска модуля:              ./vacuumdb_weekly_cluster_5432.sh
-#Прмиер запуска исполняемого модуля: ./vacuumdb_cluster.sh srv01 5432 postgres FREEZE"
+#Прмиер запуска исполняемого модуля: ./vacuumdb_cluster.sh srv01 5432 postgres 3 FREEZE"
 
 srvname=localhost
 port=5432

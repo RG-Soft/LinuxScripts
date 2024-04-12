394c15b7f07f18b3adb3dcb0e34abd3329293b2c
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-12 13:57:33 +0300

    pwd не полетел. замена метода получения директории

--- pg_scripts/chown_x_postgres.sh
+++ pg_scripts/chown_x_postgres.sh
@@ -4,7 +4,7 @@
 #
 #Пример запуска модуля: ./chown_x_postgres.sh
 
-for file_sh in `pwd`/*.sh
+for file_sh in $(dirname ${BASH_SOURCE[0]})/*.sh
 do
     chown postgres:postgres $file_sh
     chmod +x $file_sh

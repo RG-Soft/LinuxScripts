38b152255d5a6779556e1bcddb94e90f8202d1dd
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-14 17:05:58 +0300

    Подготовлены стартеры для модулей обслуживания PG

---
+++ pg_scripts/vacuumdb_daily_zakupki.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# RGS Стартер ежедневного регламента vacuumdb базы кластера
+#
+#Пример запуска стартера:            ./vacuumdb_dbname.sh 
+#Прмиер запуска исполняемого модуля: ./vacuumdb_dbase.sh srv01 5432 DBName postgres 3" 
+
+dbname=Zakupki
+srvname=localhost
+port=5432
+username=postgres
+jobs=3
+
+$(dirname ${BASH_SOURCE[0]})/vacuumdb_dbase.sh $srvname $port $dbname $username $jobs
---
+++ pg_scripts/vacuumdb_weekly_zakupki.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# RGS Стартер еженедельного регламента vacuumdb базы кластера 
+#
+#Пример запуска стартера:            ./vacuumdb_dbname.sh 
+#Прмиер запуска исполняемого модуля: ./vacuumdb_dbase.sh srv01 5432 DBName postgres 3 freeze" 
+
+dbname=Zakupki
+srvname=localhost
+port=5432
+username=postgres
+jobs=3
+freeze=freeze
+
+$(dirname ${BASH_SOURCE[0]})/vacuumdb_dbase.sh $srvname $port $dbname $username $jobs $freeze

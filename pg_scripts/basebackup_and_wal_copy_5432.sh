5617bebc919a5c88c336dfe418f1f883565128cd
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-21 16:30:37 +0300

    Добавлен стартер на регламент копирования basebackup в облако

---
+++ pg_scripts/basebackup_and_wal_copy_5432.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# RGS Стартер модуль регламента basebackup и wal файлов кластера для оформления каталога и копирования в "облоко".
+#
+#Пример запуска модуля:              ./basebackup_and_wal_copy_zakupki.sh
+#Прмиер запуска исполняемого модуля: ./basebackup_and_wal_copy.sh Zakupki /pgbackup/Zakupki/walbackup/ /pgbackup/Zakupki/basebackup/ /mnt/nas02_YANDEX/SQLBackup/Zakupki/"
+
+clustername=5432 # можно использовать номер порта, если кластен не именной, или именя базы кластера.
+walbackup_dir=/pgbackup/$clustername/walbackup/
+basebackup_dir=/pgbackup/$clustername/basebackup/
+basebackup_cloud_dir=/mnt/ru0222nas02_YANDEX/ru0994app40/SQLBackup/$clustername/
+
+$(dirname ${BASH_SOURCE[0]})/basebackup_and_wal_copy.sh $clustername $walbackup_dir $basebackup_dir $basebackup_cloud_dir

b071317e7dc3af350639ef2a25135d8307fac776
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-22 13:06:27 +0300

    Оформлено echo чтобы видеть что происходит

--- pg_scripts/_backup_copy_dbase_name.sh
+++ pg_scripts/_backup_copy_dbase_name.sh
@@ -8,10 +8,17 @@ dbasename=db_saler
 backupdir=/pgbackup/$dbasename/${dbasename}_now
 copydir=/mnt/ru0994app40_sqlbackup/${dbasename}_$(date +'%Y%m%d')
 
+echo "Запускаем копирование бэкапа базы ${dbasename} ..."
 if [ ! -d "$copydir" ]; then
+    echo -n "Каталог целевой ${copydir} не найден, создаем ... "
     mkdir -p $copydir
+    echo "Выполнено!"
 else
-   rm -f $copydir/*
+    echo -n "Каталог целевой  ${copydir} существует, запускаем зачистку перед копированием ... "
+    rm -f $copydir/*
+    echo "Выполнено!"
 fi
 
+echo -n "Запускаем копирование в ${copydir} ... "
 cp $backupdir/* $copydir/
+echo "Выполнено!"

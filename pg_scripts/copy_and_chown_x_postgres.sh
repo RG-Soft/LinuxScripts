12f4bb08e3197fa91715bb2cbf9753623b880377
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-30 00:55:01 +0300

    Исправлена ошибка, копирование не работало

--- pg_scripts/copy_and_chown_x_postgres.sh
+++ pg_scripts/copy_and_chown_x_postgres.sh
@@ -5,12 +5,12 @@
 #Пример запуска модуля: ./chown_x_postgres.sh
 
 home_pg=~postgres
-home_pgscripts="${home_pg}/pg_scripts/"
+home_pgscripts="${home_pg}/pg_scripts"
 
 for file_sh in "$(dirname ${BASH_SOURCE[0]})/*.sh"
 do
     copy_file="${home_pgscripts}/$(basename ${file_u})"
-    cp $file_u $copy_file
+    cp $file_sh $copy_file
     chown postgres:postgres $file_sh
     chmod +x $file_sh
 done
@@ -18,7 +18,7 @@ done
 for file_sh in "$(dirname ${BASH_SOURCE[0]})/main/*.sh"
 do
     copy_file="${home_pgscripts}/main/$(basename ${file_u})"
-    cp $file_u $copy_file
+    cp $file_sh $copy_file
     chown postgres:postgres $file_sh
     chmod +x $file_sh
 done

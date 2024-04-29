8d1275b572fba58f943c7dbc99657cc27ca12d83
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-30 01:21:24 +0300

    Ошибка - отсутствуюший параметр

--- pg_scripts/copy_and_chown_x_postgres.sh
+++ pg_scripts/copy_and_chown_x_postgres.sh
@@ -9,7 +9,7 @@ home_pgscripts="${home_pg}/pg_scripts"
 
 for file_sh in "$(dirname ${BASH_SOURCE[0]})/*.sh"
 do
-    copy_file="${home_pgscripts}/$(basename ${file_u})"
+    copy_file="${home_pgscripts}/$(basename ${file_sh})"
     cp $file_sh $copy_file
     chown postgres:postgres $copy_file
     chmod +x $copy_file
@@ -17,7 +17,7 @@ done
 
 for file_sh in "$(dirname ${BASH_SOURCE[0]})/main/*.sh"
 do
-    copy_file="${home_pgscripts}/main/$(basename ${file_u})"
+    copy_file="${home_pgscripts}/main/$(basename ${file_sh})"
     cp $file_sh $copy_file
     chown postgres:postgres $copy_file
     chmod +x $copy_file

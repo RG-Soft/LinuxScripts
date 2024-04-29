6d1af05b2772f5ea9138b5798e3ec6fe07b8fcb6
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-04-30 00:48:54 +0300

    как бы инсталлятор pg_scripts

---
+++ pg_scripts/copy_and_chown_x_postgres.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# RGS Скрипт для подготовки скриптов/модулей регламентов PostgreSQL.
+#
+#Пример запуска модуля: ./chown_x_postgres.sh
+
+home_pg=~postgres
+home_pgscripts="${home_pg}/pg_scripts/"
+
+for file_sh in "$(dirname ${BASH_SOURCE[0]})/*.sh"
+do
+    copy_file="${home_pgscripts}/$(basename ${file_u})"
+    cp $file_u $copy_file
+    chown postgres:postgres $file_sh
+    chmod +x $file_sh
+done
+
+for file_sh in "$(dirname ${BASH_SOURCE[0]})/main/*.sh"
+do
+    copy_file="${home_pgscripts}/main/$(basename ${file_u})"
+    cp $file_u $copy_file
+    chown postgres:postgres $file_sh
+    chmod +x $file_sh
+done

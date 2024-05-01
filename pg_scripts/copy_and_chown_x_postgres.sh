03271d52b5300d83fe2c47790174784bfe9efd09
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-05-01 22:25:45 +0300

    Добавлены опции в команду cp

--- pg_scripts/copy_and_chown_x_postgres.sh
+++ pg_scripts/copy_and_chown_x_postgres.sh
@@ -10,7 +10,7 @@ home_pgscripts=${home_pg}/pg_scripts
 for file_sh in $(dirname ${BASH_SOURCE[0]})/*.sh
 do
     copy_file=${home_pgscripts}/$(basename ${file_sh})
-    cp $file_sh $copy_file
+    cp -bvu -S bak $file_sh $copy_file
     chown postgres:postgres $copy_file
     chmod +x $copy_file
 done
@@ -18,7 +18,7 @@ done
 for file_sh in $(dirname ${BASH_SOURCE[0]})/main/*.sh
 do
     copy_file=${home_pgscripts}/main/$(basename ${file_sh})
-    cp $file_sh $copy_file
+    cp -bvu -S bak $file_sh $copy_file
     chown postgres:postgres $copy_file
     chmod +x $copy_file
 done

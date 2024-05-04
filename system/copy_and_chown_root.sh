12f3bdefada8ecd0786744c9b67b620a3653a1f6
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-05-05 00:37:26 +0300

    исправлена работа с systemctl

--- system/copy_and_chown_root.sh
+++ system/copy_and_chown_root.sh
@@ -20,7 +20,7 @@ do
         # 0 - program is running or service is OK
         # 3 - program is not running (файл сервиса есть, но сервис не enabled)
         if [[ `systemctl status $file_name --no-pager 1>/dev/null` -ne 0 ]]; then
-            echo -n "Статус enabled или running! Деактивируем ... "
+            echo "Статус enabled или running! Деактивируем."
             service_active=1
             systemctl stop $file_name # на всякий случай
             echo -n "Остановлена... "
@@ -59,7 +59,7 @@ do
         # 0 - program is running or service is OK
         # 3 - program is not running (файл сервиса есть, но сервис не enabled)
         if [[ `systemctl status $file_name --no-pager 1>/dev/null` -ne 0 ]]; then
-            echo "Статус enabled или running! Деактивируем ... "
+            echo "Статус enabled или running! Деактивируем."
             service_active=1
             systemctl stop $file_name
             echo -n "Остановлена... "

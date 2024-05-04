d86134a30287aa8b93cddbc848306081aa0bc53b
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-05-05 01:47:03 +0300

    суффикс в имени файла бэкапа исправлен

--- pg_scripts/copy_and_chown_x_postgres.sh
+++ pg_scripts/copy_and_chown_x_postgres.sh
@@ -25,7 +25,7 @@ fi
 for file_sh in $(dirname ${BASH_SOURCE[0]})/*.sh
 do
     copy_file=${home_pgscripts}/$(basename ${file_sh})
-    cp -bvu -S bak $file_sh $copy_file
+    cp -bvu -S .bak $file_sh $copy_file
     chown postgres:postgres $copy_file
     chmod +x $copy_file
 done
@@ -33,7 +33,7 @@ done
 for file_sh in $(dirname ${BASH_SOURCE[0]})/main/*.sh
 do
     copy_file=${home_pgscripts}/main/$(basename ${file_sh})
-    cp -bvu -S bak $file_sh $copy_file
+    cp -bvu -S .bak $file_sh $copy_file
     chown postgres:postgres $copy_file
     chmod +x $copy_file
 done

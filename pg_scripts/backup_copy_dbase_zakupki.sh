d8f8609e727e652141ee99346b4ca82ce5e4b164
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-05-05 03:12:52 +0300

    Подготовлены скрипты и юниты

--- pg_scripts/backup_copy_dbase_zakupki.sh
+++ pg_scripts/backup_copy_dbase_zakupki.sh
@@ -4,14 +4,25 @@
 #
 #Пример запуска стартера:            ./backup_copy_dbase_name.sh
 
+# ///////////////////////////////////////////////////////////////
+# Пользовательские параметры адаптируеются под настройки серверов
 dbasename=Zakupki
 backupdir=/pgbackup/$dbasename/${dbasename}_now
 copydir=/mnt/ru0994app40_sqlbackup/${dbasename}_$(date +'%Y%m%d')
+# ///////////////////////////////////////////////////////////////
 
+echo "Запускаем копирование бэкапа базы ${dbasename} ..."
 if [ ! -d "$copydir" ]; then
+    echo -n "Каталог целевой ${copydir} не найден, создаем ... "
     mkdir -p $copydir
+    echo "Выполнено!"
 else
-   rm -f $copydir/*
+    echo "Каталог целевой  ${copydir} существует."
+    echo -n "Запускаем зачистку перед копированием ... "
+    rm -f $copydir/*
+    echo "Выполнено!"
 fi
 
+echo -n "Запускаем копирование в ${copydir} ... "
 cp $backupdir/* $copydir/
+echo "Выполнено!"
--- pg_scripts/basebackup_and_wal_copy_zakupki.sh
+++ pg_scripts/basebackup_and_wal_copy_zakupki.sh
@@ -3,11 +3,14 @@
 # RGS Стартер модуль регламента basebackup и wal файлов кластера для оформления каталога и копирования в "облоко".
 #
 #Пример запуска модуля:              ./basebackup_and_wal_copy_zakupki.sh
-#Прмиер запуска исполняемого модуля: ./basebackup_and_wal_copy.sh Zakupki /pgbackup/Zakupki/walbackup/ /pgbackup/Zakupki/basebackup/ /mnt/nas02_YANDEX/SQLBackup/Zakupki/"
+#Прмиер запуска исполняемого модуля: ./basebackup_and_wal_copy.sh Zakupki /pgbackup/Zakupki/walbackup/ /pgbackup/Zakupki/basebackup/ /mnt/nas02_YANDEX/SQLBackup/Zakupki/
 
+# ///////////////////////////////////////////////////////////////
+# Пользовательские параметры адаптируеются под настройки серверов
 clustername=Zakupki # можно использовать номер порта, если кластен не именной, или именя базы кластера.
 walbackup_dir=/pgbackup/$clustername/walbackup/
 basebackup_dir=/pgbackup/$clustername/basebackup/
 basebackup_cloud_dir=/mnt/ru0222nas02_YANDEX/ru0994app40/SQLBackup/$clustername/
+# ///////////////////////////////////////////////////////////////
 
-$(dirname ${BASH_SOURCE[0]})/basebackup_and_wal_copy.sh $clustername $walbackup_dir $basebackup_dir $basebackup_cloud_dir
+$(dirname ${BASH_SOURCE[0]})/main/basebackup_and_wal_copy.sh $clustername $walbackup_dir $basebackup_dir $basebackup_cloud_dir

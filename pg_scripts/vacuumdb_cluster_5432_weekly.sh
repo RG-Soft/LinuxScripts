d5f5d1f2c0d5d8505ddf6a7c98fac59059268352
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-05-06 00:52:28 +0300

    обновлены стартеры под новый релиз

--- pg_scripts/vacuumdb_cluster_5432_weekly.sh
+++ pg_scripts/vacuumdb_cluster_5432_weekly.sh
@@ -2,13 +2,23 @@
 #
 # RGS Стартер модуля регламента vacuumdb еженедельного для всех баз кластера.
 #
-#Пример запуска модуля:              ./vacuumdb_cluster_5432_weekly.sh
-#Прмиер запуска исполняемого модуля: ./vacuumdb_cluster.sh srv01 5432 postgres 3 FREEZE"
+# Пример запуска модуля:              ./vacuumdb_cluster_5432_weekly.sh
+# Прмиер запуска исполняемого модуля: ./vacuumdb_cluster.sh srv01 5432 postgres 3 "base1 base3" FREEZE
 
+# ///////////////////////////////////////////////////////////////
+# Пользовательские параметры адаптируеются под настройки серверов
 srvname=localhost
 port=5432
 username=postgres
 jobs=3
+skip_userdblist=""
 freeze=FREEZE
+# ///////////////////////////////////////////////////////////////
 
-$(dirname ${BASH_SOURCE[0]})/vacuumdb_cluster.sh $srvname $port $username $jobs $freeze
+# ///////////////////////////////////////////////////////////////
+# Параметры системные, править не требуется
+skip_systemdblist="\N template0 template1 postgres"
+# ///////////////////////////////////////////////////////////////
+
+skip_dblist="${skip_systemdblist} ${skip_userdblist}"
+$(dirname ${BASH_SOURCE[0]})/main/vacuumdb_cluster.sh $srvname $port $username $jobs "$skip_dblist" $freeze

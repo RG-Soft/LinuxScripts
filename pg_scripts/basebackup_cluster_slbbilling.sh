600b5f18faa962c7aecef0519e82abe2ae5129a7
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-05-05 13:06:47 +0300

    Обновлено на запрет перезазписи по параметру

--- pg_scripts/basebackup_cluster_slbbilling.sh
+++ pg_scripts/basebackup_cluster_slbbilling.sh
@@ -3,7 +3,7 @@
 # RGS Стартер модуль регламента basebackup для кластера.
 #
 #Пример запуска модуля:              ./basebackup_cluster_5432.sh
-#Прмиер запуска исполняемого модуля: ./basebackup_cluster.sh srv01 5432 postgres db_saler /pgbackup
+#Прмиер запуска исполняемого модуля: ./basebackup_cluster.sh srv01 5432 postgres db_saler /pgbackup NO
 
 # ///////////////////////////////////////////////////////////////
 # Пользовательские параметры адаптируеются под настройки серверов
@@ -12,6 +12,7 @@ srvname=localhost
 port=5434
 username=postgres
 backupdir_root=/pgbackup
+backupdir_rewrite=NO #YES включает удаление старого бэкапа. Удаление хвостов протерянных WAL файлов - вручную
 # ///////////////////////////////////////////////////////////////
 
-$(dirname ${BASH_SOURCE[0]})/main/basebackup_cluster.sh $srvname $port $username $clustername $backupdir_root
+$(dirname ${BASH_SOURCE[0]})/main/basebackup_cluster.sh $srvname $port $username $clustername $backupdir_root $backupdir_rewrite

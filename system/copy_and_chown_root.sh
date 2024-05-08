4b61a2a66aea320781dac61b0896be24fe9fa76e
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-05-09 01:08:52 +0300

    Обновлен модуль, логика новая

--- system/copy_and_chown_root.sh
+++ system/copy_and_chown_root.sh
@@ -10,18 +10,18 @@ do
 
     file_name=$(basename ${file_u})
     copy_file=/etc/systemd/system/$file_name
-    file_present=0
-    service_active=0
+    service_file_present=0
+    #service_active=0
 
     if [ -f "$copy_file" ]; then
         echo -n "Найден сервис юнит $file_name! ... "
-        file_present=1
+        service_file_present=1
 
         # 0 - program is running or service is OK
         # 3 - program is not running (файл сервиса есть, но сервис не enabled)
         if [[ `systemctl status $file_name --no-pager 1>/dev/null` -ne 0 ]]; then
             echo -n "Статус enabled или running! Деактивируем ... "
-            service_active=1
+            #service_active=1
             systemctl stop $file_name # на всякий случай
             echo -n "Остановлена... "
             systemctl disable $file_name #такой юнит не должен быть enable
@@ -35,11 +35,13 @@ do
         echo "Юнит скопирован в каталог system"
         chown root:root $copy_file
 
-        if [[ $file_present -eq 1 ]]; then
-            echo -n "Файл юнита был ранее в system, будет перезагружена конфигурация systemd ... "
-            systemctl daemon-reload
-            echo "Выполнено!"
-        fi
+        #if [[ $service_file_present -eq 1 ]]; then
+        #    echo -n "Файл юнита был ранее в system, будет перезагружена конфигурация systemd ... "
+        #    systemctl daemon-reload
+        #    echo "Выполнено!"
+        #fi
+    else
+        echo "Ошибка копирования юнита сервиса ${copy_file}..."
     fi
     echo
 done
@@ -50,44 +52,46 @@ do
 
     file_name=$(basename ${file_u})
     copy_file=/etc/systemd/system/$file_name
-    file_present=0
+    timer_file_present=0
+    service_active=0
 
     if [ -f "$copy_file" ]; then
         echo -n "Найден тайм юнит $file_name! ... "
-        file_present=1
+        timer_file_present=1
 
         # 0 - program is running or service is OK
         # 3 - program is not running (файл сервиса есть, но сервис не enabled)
         if [[ `systemctl status $file_name --no-pager 1>/dev/null` -ne 0 ]]; then
-            echo "Статус enabled или running! Деактивируем ... "
+            #echo "Статус enabled или running! Деактивируем ... "
             service_active=1
-            systemctl stop $file_name
-            echo -n "Остановлена... "
-            systemctl disable $file_name
-            echo "Выключена. "
+            #systemctl stop $file_name
+            #echo -n "Остановлена... "
+            #systemctl disable $file_name
+            #echo "Выключена. "
         else
             echo "Статус disbled или not running!"
         fi
     fi
 
     if cp $file_u $copy_file; then
-         echo "Юнит скопирован в каталог system"
+        echo "Юнит скопирован в каталог system"
         chown root:root $copy_file
 
-        if [[ $file_present -eq 1 ]]; then
-            echo "Файл юнита был ранее в system, будет перезагружена конфигурация systemd"
+        if [[ $service_file_present -eq 1 ]] || [[ $timer_file_present -eq 1 ]]; then
+            echo "Файлы юнитов были ранее в system, будет перезагружена конфигурация systemd"
             systemctl daemon-reload
+        else
+            echo -n "Запускаем и активируем (enable) тайм юнит ... "
+            systemctl enable $file_name
+            echo -n "Включена ... "
+            systemctl start $file_name
+            echo "Запущена."
+            if [[ `systemctl status $file_name --no-pager 1>/dev/null` -ne 0 ]]; then
+                echo "Ошибка! Тайм юниту не установлен enable статус."
+            fi
         fi
-
-        echo -n "Запускаем и активируем (enable) тайм юнит ... "
-        systemctl enable $file_name
-        echo -n "Включена ... "
-        systemctl start $file_name
-        echo "Запущена."
-        if [[ `systemctl status $file_name --no-pager 1>/dev/null` -ne 0 ]]; then
-            echo "Ошибка! Тайм юниту не установлен enable статус."
-        fi
+    else
+        echo "Ошибка копирования юнита таймера ${copy_file}..."
     fi
     echo
-
 done

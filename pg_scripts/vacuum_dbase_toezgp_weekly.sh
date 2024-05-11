4450467474294921f3b858928d22b502b4d70155
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-05-11 22:55:07 +0300

    Имена модулей к типовому формату и исправлен косяк basebackup

---
+++ pg_scripts/vacuum_dbase_toezgp_weekly.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# RGS Стартер модуля регламента vacuumdb базы кластера 
+#
+# Пример запуска стартера:            ./vacuum_dbase_name_weekly.sh 
+# Прмиер запуска исполняемого модуля: ./vacuum_dbase.sh srv01 5432 DBName postgres 3 freeze" 
+
+# ///////////////////////////////////////////////////////////////
+# Пользовательские параметры адаптируеются под настройки серверов
+dbname=TOEZGP
+srvname=localhost
+port=5432
+username=postgres
+jobs=3
+freeze=FREEZE
+# ///////////////////////////////////////////////////////////////
+
+$(dirname ${BASH_SOURCE[0]})/main/vacuum_dbase.sh $srvname $port $dbname $username $jobs $freeze

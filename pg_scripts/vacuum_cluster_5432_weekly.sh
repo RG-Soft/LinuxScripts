0f017a4d7eb60888fc07d1ab0e135adb4e480dcd
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-05-11 23:47:27 +0300

    Привелено к типовому и ошибки

--- pg_scripts/vacuum_cluster_5432_weekly.sh
+++ pg_scripts/vacuum_cluster_5432_weekly.sh
@@ -2,8 +2,8 @@
 #
 # RGS Стартер модуля регламента vacuumdb еженедельного для всех баз кластера.
 #
-# Пример запуска модуля:              ./vacuumdb_cluster_5432_weekly.sh
-# Прмиер запуска исполняемого модуля: ./vacuumdb_cluster.sh srv01 5432 postgres 3 "base1 base3" FREEZE
+# Пример запуска модуля:              ./vacuum_cluster_5432_weekly.sh
+# Прмиер запуска исполняемого модуля: ./vacuum_cluster.sh srv01 5432 postgres 3 "base1 base3" FREEZE
 
 # ///////////////////////////////////////////////////////////////
 # Пользовательские параметры адаптируеются под настройки серверов
@@ -21,4 +21,4 @@ skip_systemdblist="\N template0 template1 postgres"
 # ///////////////////////////////////////////////////////////////
 
 skip_dblist="${skip_systemdblist} ${skip_userdblist}"
-$(dirname ${BASH_SOURCE[0]})/main/vacuumdb_cluster.sh $srvname $port $username $jobs "$skip_dblist" $freeze
+$(dirname ${BASH_SOURCE[0]})/main/vacuum_cluster.sh $srvname $port $username $jobs "$skip_dblist" $freeze

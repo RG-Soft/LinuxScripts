646ec006cf1a93f82f66abde153635f8ad187996
VladislavChaplygin <vchaplygin@rg-spc.ru>
2024-09-23 22:12:19 +0300

    Изменен график, сначала analize для более точного определения мертвых строк, потом repack и вместо 2 потоков - 4 потока

--- pg_scripts/vacuum_dbase_slbsvs_acc_daily.sh
+++ pg_scripts/vacuum_dbase_slbsvs_acc_daily.sh
@@ -11,7 +11,7 @@ dbname=slbSVS_acc
 srvname=localhost
 port=5434
 username=postgres
-jobs=2
+jobs=4
 # ///////////////////////////////////////////////////////////////
 
 $(dirname ${BASH_SOURCE[0]})/main/vacuum_dbase.sh $srvname $port $dbname $username $jobs

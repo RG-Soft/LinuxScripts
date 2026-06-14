311f82219c8c58f593bed5685f1729679d62052f
VladislavChaplygin <vchaplygin@rg-spc.ru>
2026-06-15 02:07:07 +0500

    Обновлены каталоги хранения в облаке

--- pg_scripts/backup_copy_dir_zakupki.sh
+++ pg_scripts/backup_copy_dir_zakupki.sh
@@ -8,7 +8,12 @@
 # Пользовательские параметры адаптируеются под настройки серверов
 dbname=Zakupki
 backupdir=/pgbackup/$dbname/${dbname}_now
-copydir=/mnt/ru0222nas02_YANDEX/ru0994app40/SQLBackup/${dbname}/${dbname}_$(date +'%Y%m%d')
+
+# История бэкапов ведется, бэкап сохраняется в каталог с меткой времени
+#copydir=/mnt/ru0149bck01_SQLBackups/ru0994app40/SQLBackup/${dbname}/${dbname}_$(date +'%Y%m%d')
+
+# История бэкапов не ведется, бэкап используется для перезаливки баз разработчиков
+ copydir=/mnt/ru0149bck01_SQLBackups/ru0222app48/SQLBackup/${dbname}/${dbname}_now
 # ///////////////////////////////////////////////////////////////
 
 $(dirname ${BASH_SOURCE[0]})/main/backup_copy_dir.sh $dbname $backupdir $copydir

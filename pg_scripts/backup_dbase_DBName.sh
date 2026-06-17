a5abbc503520fedc2e68dc6be3258b6d4f176043
VladislavChaplygin <vchaplygin@rg-spc.ru>
2026-06-17 22:21:33 +0300

    Удалены лишние файлы и исправлены параметры и имена на обезличенные

--- pg_scripts/backup_dbase_DBName.sh
+++ pg_scripts/backup_dbase_DBName.sh
@@ -7,15 +7,15 @@
 
 # ///////////////////////////////////////////////////////////////
 # Пользовательские параметры адаптируеются под настройки серверов
-dbname=CIET_CAS_QA
-srvname=129.87.65.211
+dbname=DBName
+srvname=SRVNAME
 port=5433
 username=postgres
-jobs=3
-backupdir_root=/pgbackup
-#backupdir_root=/mnt/pgbackup/ru0222app48
-#backup_suffix=_now # История бэкапов не ведется, бэкап используется для перезаливки баз разработчиков
-backup_suffix=$(date +'%Y%m%d_%H%M%S') # История бэкапов ведется, бэкап сохраняется в каталог с меткой времени
+jobs=4
+backupdir_root=/pgbackup                    # Локальный путь
+#backupdir_root=/mnt/pgbackup/ru0222app48   # Сетевое хранилище
+backup_suffix=_now                          # История бэкапов не ведется, бэкап используется для перезаливки баз разработчиков
+#backup_suffix=$(date +'%Y%m%d_%H%M%S')     # История бэкапов ведется, бэкап сохраняется в каталог с меткой времени
 # ///////////////////////////////////////////////////////////////
 
 $(dirname ${BASH_SOURCE[0]})/main/backup_dbase.sh $srvname $port $dbname $username $jobs $backupdir_root $backup_suffix

62f9695d0446932b47962f6a6a6d45da1806b55a
VladislavChaplygin <vchaplygin@rg-spc.ru>
2026-06-21 14:20:52 +0300

    Восстановлены pg скрипты после ходожеств Александра

---
+++ pg_scripts/repack_cluster_zakupki.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# RGS Стартер регламента repack всех баз кластера. НО ТОЛЬКО "убитых" таблиц и индексов
+#
+# Пример запуска стартера:            ./repack_cluster_5432.sh
+# Прмиер запуска исполняемого модуля: ./repack_cluster.sh srv01 5432 postgres 3 "base1 base3"
+
+# ///////////////////////////////////////////////////////////////
+# Пользовательские параметры адаптируеются под настройки серверов
+srvname=localhost
+port=5432
+username=postgres
+jobs=3
+skip_userdblist=""
+# ///////////////////////////////////////////////////////////////
+
+# ///////////////////////////////////////////////////////////////
+# Параметры системные, править не требуется
+skip_systemdblist="\N template0 template1 postgres"
+# ///////////////////////////////////////////////////////////////
+
+skip_dblist="${skip_systemdblist} ${skip_userdblist}"
+$(dirname ${BASH_SOURCE[0]})/main/repack_cluster.sh $srvname $port $username $jobs "$skip_dblist"
---
+++ pg_scripts/vacuum_dbase_zakupki_daily.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# RGS Стартер модуля регламента vacuumdb ежедневного vacuumdb базы кластера
+#
+# Пример запуска стартера:            ./vacuum_dbase_name_daily.sh 
+# Прмиер запуска исполняемого модуля: ./vacuum_dbase.sh srv01 5432 DBName postgres 3" 
+
+# ///////////////////////////////////////////////////////////////
+# Пользовательские параметры адаптируеются под настройки серверов
+dbname=Zakupki
+srvname=localhost
+port=5432
+username=postgres
+jobs=3
+# ///////////////////////////////////////////////////////////////
+
+$(dirname ${BASH_SOURCE[0]})/main/vacuum_dbase.sh $srvname $port $dbname $username $jobs
